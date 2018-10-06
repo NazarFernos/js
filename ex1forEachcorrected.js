c68c114e5ec237527ccb6524037dea09be36a412
Nazar Fernos <nazarum98@gmail.com>
2018-10-06 15:12:03 +0200

    Update and rename ex1.1.js to ex1forEachcorrected.js

--- ex1forEachcorrected.js
+++ ex1forEachcorrected.js
@@ -1,13 +1,15 @@
-const arry = [1,2,3,4,5];
+//corrected
 var array = [3,99,55,100,25];
 var arraySum = 0;
+
 function getSums (arr) {
-	let result = [];
-	if (!arr.length) return result;
+    let result = [];
+    if (!arr.length) return result;
 array.forEach( function (item) {
 	arraySum += item;
 }) ;
-	let totalSum = arr.reduce (function (sum, item) {
+
+var totalSum = arr.reduce (function (sum, item) {
 		result.push( sum );
 		return sum + item;
 	});
