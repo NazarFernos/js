9323760f0aac9ee923d0399346803735c6110db7
Nazar Fernos <nazarum98@gmail.com>
2018-10-06 23:17:14 +0200

    Delete ex3.js

--- ex3.js
+++
@@ -1,7 +0,0 @@
-var arr = [1,3,5,8,3,1];
-
-function returnUniqueVal (value, index, self) {
-	return self.indexOf ( value ) == index;
-	return result;
-}
-console.log( arr.filter ( returnUniqueVal) );
