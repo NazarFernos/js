ba32b8c0a75d3076e33430c297317d8940d660ef
Nazar Fernos <nazarum98@gmail.com>
2018-10-07 16:35:06 +0200

    Add files via upload

---
+++ task1forEach.js
@@ -0,0 +1,11 @@
+var arr = [1, 4, 3, 6, 9];
+var arrSum = 0;
+
+arr.forEach(function(item) {
+   arrSum += item;
+   });
+console.log(arrSum);
+
+//item – очередной элемент массива.
+//i – его номер.
+//arr – массив, который перебирается
