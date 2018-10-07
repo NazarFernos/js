7b88f46ecedbe790753d857b388c0fdb016fcc70
Nazar Fernos <nazarum98@gmail.com>
2018-10-07 23:05:16 +0200

    Add files via upload

---
+++ task2for.js
@@ -0,0 +1,11 @@
+var arr = [122, 1, 4, 3, 55, 6, 9, 2];
+
+for (var i = 1; i < arr.length; i++)
+    for (var a = 0; a < i; a++)
+        if (arr [i] < arr [a]) {
+          var b = arr [i];
+          arr [i] = arr [a];
+          arr [a] = b;
+        }
+
+console.log( arr );
