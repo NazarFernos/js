65902128c8aeaf09d6f0dc9f98a85cfa6070e2ab
Nazar Fernos <nazarum98@gmail.com>
2018-10-11 23:50:38 +0200

    Update task1for.js

--- task1for.js
+++ task1for.js
@@ -1,5 +1,9 @@
-var arr = [122, 1, 4, 3, 55, 6, 9, 2];
-var sumArr = 0;
-for (i=0; i<arr.length; i++)
-    sumArr += arr[i];
-console.log( sumArr );
+function getArraySum(arr) {
+  var arr = [122, 1, 4, 3, 55, 6, 9, 2];
+  var arrSum = 0;
+
+  for (i=0; i<arr.length; i++)
+    arrSum += arr[i];
+  return arrSum;
+}
+console.log(getArraySum([122, 1, 4, 3, 55, 6, 9, 2]));
