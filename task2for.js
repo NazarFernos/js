e502305298289f0d236bdf1881e3c1de622a43e8
Nazar Fernos <nazarum98@gmail.com>
2018-10-14 01:34:37 +0200

    Update task2for.js

--- task2for.js
+++ task2for.js
@@ -1,11 +1,13 @@
-var arr = [122, 1, 4, 3, 55, 6, 9, 2];
+function getSortedArray(arr) {
+    var arr = [122, 0, 1, 4, 3, 55, 6, 9, 2];
 
-for (var i = 1; i < arr.length; i++)
-    for (var a = 0; a < i; a++)
-        if (arr [i] < arr [a]) {
-          var b = arr [i];
-          arr [i] = arr [a];
-          arr [a] = b;
-        }
-
-console.log( arr );
+  for (var i = 1; i < arr.length; i++)
+      for (var a = 0; a < i; a++)
+          if (arr [i] < arr [a]) {
+            var b = arr [i];
+            arr [i] = arr [a];
+            arr [a] = b;
+          }
+return arr;
+}
+console.log(getSortedArray([122, 0, 1, 4, 3, 55, 6, 9, 2]));
