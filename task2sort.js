d5cae0e000d18ab962f3737e237ad0145603e794
Nazar Fernos <nazarum98@gmail.com>
2018-10-16 22:09:37 +0200

    Update task2sort.js

--- task2sort.js
+++ task2sort.js
@@ -1,6 +1,10 @@
 function getSortedArray(a, b) {
-    if (a > b) return 1;
-    if (a < b) return -1;
+  if (a > b) {
+  return 1;
+  };
+  if (a < b) {
+  return -1;
+  };
 
   var arr = [122, 1, 4, 3, 55, 6, 9, 2];
   arr.sort(getSortedArray);
