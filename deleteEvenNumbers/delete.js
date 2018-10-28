d308ddce8be178e240f542e1adc767c618b9a2ab
Nazar Fernos <nazarum98@gmail.com>
2018-10-28 17:37:57 +0100

    Create delete.js

---
+++ deleteEvenNumbers/delete.js
@@ -0,0 +1,10 @@
+function deleteEvenNumbers(arr) {
+
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i] % 2 === 0) {
+            delete arr[i];
+        }
+    }
+    return arr;
+}
+console.log(deleteEvenNumbers([1,2,3,4,5,6,7,8,9]));
