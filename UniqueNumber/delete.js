76e82275b4ac0f96339b3d40a0fd1c3c031d37fd
Nazar Fernos <nazarum98@gmail.com>
2018-11-28 01:12:12 +0100

    Update delete.js

--- UniqueNumber/delete.js
+++ UniqueNumber/delete.js
@@ -12,10 +12,11 @@ function isNumberNonDuplicating(n, arr) {
 
     var count = 0;
     for(var i = 0; i < arr.length; i++) {
-        if(n == arr[i]) {
+        if(n === arr[i]) {
             count++;
         }
     }
     return count < 2;
 }
-console.log(deleteUnSameNumber([7,5,0,1,2,2,3,5,3,3,0,4,4,-2,-2,-80])); //[5, 0, 2, 2, 3, 5, 3, 3, 0, 4, 4]
+console.log(deleteUnSameNumber([7,5,0,1,2,2,3,5,3,3,0,4,4,-2,-2,-80])); 
+//[5, 0, 2, 2, 3, 5, 3, 3, 0, 4, 4]
