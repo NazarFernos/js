2c71dc1312c6f33f9f5701a042603ef277c8e78c
Nazar Fernos <nazarum98@gmail.com>
2018-11-28 01:09:39 +0100

    Create delete.js

---
+++ UniqueNumber/delete.js
@@ -0,0 +1,21 @@
+function deleteUnSameNumber(arr) {
+
+    for(var i = 0; i < arr.length; i++) {
+        if(isNumberNonDuplicating(arr[i], arr)) {
+            delete arr[i];
+        } 
+    }
+    return arr.filter(n => n >= 0);
+}
+
+function isNumberNonDuplicating(n, arr) {
+
+    var count = 0;
+    for(var i = 0; i < arr.length; i++) {
+        if(n == arr[i]) {
+            count++;
+        }
+    }
+    return count < 2;
+}
+console.log(deleteUnSameNumber([7,5,0,1,2,2,3,5,3,3,0,4,4,-2,-2,-80]));
