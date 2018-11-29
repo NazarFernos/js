cbe3a79bc9aad2972c40992d234f3b6541a2fc02
Nazar Fernos <nazarum98@gmail.com>
2018-11-29 17:46:08 +0100

    Update spread.js

--- combineArrays/spread.js
+++ combineArrays/spread.js
@@ -1,6 +1,6 @@
 function combineArrays(arr) {
     var arrTwo = [4,5,6];
-    var arr = [1,2,3, ...arrTwo];
-    return arr;
+    var arr2 = [...arr, ...arrTwo];
+    return arr2;
 }
 console.log(combineArrays([1,2,3]));
