552d7b37e295d1867d9b28d64a827d9e02e0606c
Nazar Fernos <nazarum98@gmail.com>
2018-12-05 22:03:18 +0100

    Create for.js

---
+++ Strings/countWordsInString/for.js
@@ -0,0 +1,11 @@
+function countWordsInString(str) {
+    var count = 0;
+
+    for(var i = 0; i < str.length; i++) {
+        if(str[i] === ",") {
+            count ++;
+        }
+    }
+    return count + 1;
+}
+console.log(countWordsInString("first, second, third"));
